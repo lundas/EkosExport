12d47fa2482b645640f00ee38e4855b1e804752b
Andrew Lund <andrew.s.lund@gmail.com>
2022-01-30 21:57:00 -0800

    added/edited function comment strings to functions

--- src/ekosexport.py
+++ src/ekosexport.py
@@ -15,224 +15,252 @@ from selenium.common.exceptions import NoSuchFrameException
 from selenium.common.exceptions import ElementClickInterceptedException
 
 class EkosExport:
-	'''Class for accessing and downloading items from Ekos ERP using Selenium
-	Webdriver.
+    '''Class for accessing and downloading items from Ekos ERP using Selenium
+    Webdriver.
 
-	Requires geckodriver for Firefox, ChromeDriver for Chrome
+    Requires geckodriver for Firefox, ChromeDriver for Chrome
+    **CURRENTLY ONLY IMPLEMENTED FOR FIREFOX**
 
-	browser
-	profile
-	profile_path
-	'''
+    PARAMS
+    --------------
+    browser : Selenium Web Browser to be used. Requires browser and the 
+    correct webdriver to be installed on the machine
 
-	def __init__(
-		self,
-		browser,
-		driver_path,
-		profile_dir,
-		profile_dir_path,
-		headless=False
-	):
-		self.browser = browser
-		self.driver_path = driver_path
-		self.profile_dir = profile_dir
-		self.profile_dir_path = profile_dir_path
-		self.headless = headless
+        Firefox --> geckodriver
+        Chrome --> ChromeDriver
 
-		if self.browser.lower() == 'firefox':
-			#set profile
-			self.profile = FirefoxProfile()
-			#set profile directory preference
-			self.profile.set_preference(
-				'browser.download.folderList', 
-				self.profile_dir
-				)
-				#if profile_dir set to custom, set path to dir
-			if self.profile_dir == 2:
-				self.profile.set_preference(
-					'browser.download.dir', 
-					self.profile_dir_path
-					)
-			self.profile.set_preference(
-				'browser.helperApps.neverAsk.openFile',
-				'text/csv,application/vnd.ms-excel'
-				)
-			self.profile.set_preference(
-				'browser.helperApps.neverAsk.saveToDisk',
-				'text/csv,application/vnd.ms-excel'
-				)
+        **CURRENTLY ONLY IMPLEMENTED FOR FIREFOX. CHROME WILL THROW AN ERROR**
 
-			#set options
-			self.options = Options()
-			if self.headless == True:
-				self.options.add_argument('-headless')
-				self.options.set_headless
+    driver_path : Location of the webdriver required for the selected browser
+        
+        e.g. /PATH/to/geckodriver for Firefox
 
-			self.session = webdriver.Firefox(
-				firefox_profile=self.profile,
-				executable_path=self.driver_path,
-				options=self.options
-			)
-			# wait for web elements to load
-			self.session.implicitly_wait(10)
+    profile_dir : Location where files from the session will be downloaded on
+    the local machine
 
-	def login(self, session, username, password):
-		''' Logs in to Ekos using credential provided by user and handles
-		any alerts that may occur during log in
+        1 == tmp/
+        2 == custom directory provided. Location provided by profile_dir_path
+        3 == USERPROFILE
 
-		PARAMS
-		-----------
-		session : selenium webdriver session iniated using open session funciton
-		username : ekos ERP username
-		password : ekos ERP password
-		'''
-		#open webdriver, go to Ekos login page
-		print('Logging into Ekos')
-		session.get('https://login.goekos.com/')
-		assert 'Ekos' in session.title
-		# enter login credentials
-		elem = session.find_element_by_id('txtUsername')
-		elem.send_keys(username)
-		elem = session.find_element_by_id('txtPassword')
-		elem.send_keys(password)
-		elem.send_keys(Keys.RETURN)
+        For more information, refer to Docs
+        'https://firefox-source-docs.mozilla.org/testing/geckodriver/Profiles.html'
 
-		print('Login Successful')
+    profile_dir_path : Location of download directory if setting custom directory
 
-		# session.implicitly_wait(10) #wait for page to load
+    headless : determines whether or not to run Selenium in headless mode, 
+    which is necessary for running on a machine without a monitor e.g. in 
+    the cloud
+    '''
+    def __init__(
+        self,
+        browser,
+        driver_path,
+        profile_dir,
+        profile_dir_path,
+        headless=False
+    ):
+        self.browser = browser
+        self.driver_path = driver_path
+        self.profile_dir = profile_dir
+        self.profile_dir_path = profile_dir_path
+        self.headless = headless
 
-		return
+        if self.browser.lower() == 'firefox':
+            #set profile
+            self.profile = FirefoxProfile()
+            #set profile directory preference
+            self.profile.set_preference(
+                'browser.download.folderList', 
+                self.profile_dir
+                )
+                #if profile_dir set to custom, set path to dir
+            if self.profile_dir == 2:
+                self.profile.set_preference(
+                    'browser.download.dir', 
+                    self.profile_dir_path
+                    )
+            self.profile.set_preference(
+                'browser.helperApps.neverAsk.openFile',
+                'text/csv,application/vnd.ms-excel'
+                )
+            self.profile.set_preference(
+                'browser.helperApps.neverAsk.saveToDisk',
+                'text/csv,application/vnd.ms-excel'
+                )
 
-	def download_report(self, session, report_name):
-		'''Clicks report name and downloads report as csv
+            #set options
+            self.options = Options()
+            if self.headless == True:
+                self.options.add_argument('-headless')
+                self.options.set_headless
 
-		PARAMS
-		-----------
-		session : Selenium webdriver session returned by open_session function
-		report_name : report to be downloaded from ekos reports page
-		'''
+            self.session = webdriver.Firefox(
+                firefox_profile=self.profile,
+                executable_path=self.driver_path,
+                options=self.options
+            )
+            # wait for web elements to load
+            self.session.implicitly_wait(10)
 
-		# session.implicitly_wait(10)
+    def login(self, session, username, password):
+        ''' Logs in to Ekos using credential provided by user and handles
+        any alerts that may occur during log in
 
-		# get reports page by url
-		# print('navigating to reports page')
-		# session.get('https://app.goekos.com/03.00/Report_Category') # TODO: user input url?
+        PARAMS
+        -----------
+        session : selenium webdriver session iniated using open session funciton
+        username : ekos ERP username
+        password : ekos ERP password
+        '''
+        #open webdriver, go to Ekos login page
+        print('Logging into Ekos')
+        session.get('https://login.goekos.com/')
+        assert 'Ekos' in session.title
+        # enter login credentials
+        elem = session.find_element_by_id('txtUsername')
+        elem.send_keys(username)
+        elem = session.find_element_by_id('txtPassword')
+        elem.send_keys(password)
+        elem.send_keys(Keys.RETURN)
 
-		# Navigate to reports page
-		print('navigating to reports page')
-		elem = session.find_element_by_xpath(
-			# select 4th button in nav-options div
-			"//div[@class='nav-options']/button[4]"
-		)
-		elem.click()
-		elem = session.find_element_by_xpath(
-			# select first link -- Report Category
-			"//div[@class='nav-option--group']/a[1]")
-		elem.click()
+        print('Login Successful')
 
-		# Reports page is Ekos Classic iFrame
-		# Switch to iFrame
-		print('switching to iFrame')
-		session.switch_to.frame('classicContainer')
-		
-		# find link by link text
-		print('opening report name: {}'.format(report_name))
-		# elem = WebDriverWait(session, 5).until(
-		# 	EC.element_to_be_clickable((By.LINK_TEXT, report_name))
-		# )
-		elem = session.find_element_by_link_text(report_name)
-		elem.click()
+        # session.implicitly_wait(10) #wait for page to load
 
-		# switch to iframe
-		print('switching to iFrame')
-		session.switch_to.frame('formFrame_0')
+        return
 
-		# click export button
-		print('clicking export button')
-		elem = session.find_element_by_class_name('buttonGroupInner')
-		elem.click()
+    def download_report(self, session, report_name):
+        '''Clicks report name and downloads report as csv
 
-		# download report as csv
-		print('downloading report as csv to {}'.format(self.profile_dir_path))
-		elem = session.find_element_by_id('csv_export')
-		elem.click()
+        PARAMS
+        -----------
+        session : Selenium webdriver session returned by open_session function
+        report_name : report to be downloaded from ekos reports page
+        '''
 
-		# close iFrame
-		print('switching to default content')
-		session.switch_to.default_content()
-		print('switching to iFrame')
-		session.switch_to.frame('classicContainer')
-		print('closing iFrame')
-		elem=session.find_element_by_class_name('formClose')
-		elem.click()
+        # session.implicitly_wait(10)
 
-		return
+        # get reports page by url
+        # print('navigating to reports page')
+        # session.get('https://app.goekos.com/03.00/Report_Category') # TODO: user input url?
 
-	def quit(self, session):
-		'''Quits session opened by open_session
+        # Navigate to reports page
+        print('navigating to reports page')
+        elem = session.find_element_by_xpath(
+            # select 4th button in nav-options div
+            "//div[@class='nav-options']/button[4]"
+        )
+        elem.click()
+        elem = session.find_element_by_xpath(
+            # select first link -- Report Category
+            "//div[@class='nav-option--group']/a[1]")
+        elem.click()
 
-		PARAMS
-		---------
-		session : Selenium webdriver session returned by open_session function
-		'''
-		session.quit()
-		return
+        # Reports page is Ekos Classic iFrame
+        # Switch to iFrame
+        print('switching to iFrame')
+        session.switch_to.frame('classicContainer')
+        
+        # find link by link text
+        print('opening report name: {}'.format(report_name))
+        # elem = WebDriverWait(session, 5).until(
+        #   EC.element_to_be_clickable((By.LINK_TEXT, report_name))
+        # )
+        elem = session.find_element_by_link_text(report_name)
+        elem.click()
 
-	def rename_file(
-		self, 
-		new_filename,
-		regex='Export_\d{14}_\.csv', #default ekos file name 
-		PATH=None #None defaults to self.profile_dir_path
-	):
-		'''Searches for the downloaded csv file based on a regular expression
-		and replaces that filename with the filename provided
+        # switch to iframe
+        print('switching to iFrame')
+        session.switch_to.frame('formFrame_0')
 
-		PARAMS
-		---------
-		new_filename : new filename for the downloaded file
-		regex : regular expression to search for
-		PATH : path to directory to search using regex
-		'''
-		if PATH == None:
-			PATH = self.profile_dir_path
+        # click export button
+        print('clicking export button')
+        elem = session.find_element_by_class_name('buttonGroupInner')
+        elem.click()
 
-		regex = re.compile(regex)
-		count = 0 #count to track len of os.listdir(PATH)
-		for f in os.listdir(PATH):
-			count += 1
-			if regex.match(f) != None:
-				print('File Found!')
-				filename = regex.match(f).string
-				os.rename(PATH+filename, PATH+new_filename)
-				break
-			elif regex.match(f) == None and count == len(os.listdir(PATH)):
-				print('File not found')
-			else:
-				print('File does not match regex. Checking next file')
-		return
+        # download report as csv
+        print('downloading report as csv to {}'.format(self.profile_dir_path))
+        elem = session.find_element_by_id('csv_export')
+        elem.click()
+
+        # close iFrame
+        print('switching to default content')
+        session.switch_to.default_content()
+        print('switching to iFrame')
+        session.switch_to.frame('classicContainer')
+        print('closing iFrame')
+        elem=session.find_element_by_class_name('formClose')
+        elem.click()
+
+        return
+
+    def quit(self, session):
+        '''Quits session opened by open_session
+
+        PARAMS
+        ---------
+        session : Selenium webdriver session returned by open_session function
+        '''
+        session.quit()
+        return
+
+    def rename_file(
+        self, 
+        new_filename,
+        regex='Export_\d{14}_\.csv', #default ekos file name format
+        PATH=None # None defaults to self.profile_dir_path
+    ):
+        '''Searches for the downloaded csv file based on a regular expression
+        and replaces that filename with the filename provided
+
+        PARAMS
+        ---------
+        new_filename : new filename for the downloaded file
+
+        regex : regular expression to search for
+
+        PATH : path to directory to search using regex
+        '''
+        if PATH == None:
+            PATH = self.profile_dir_path
+
+        regex = re.compile(regex)
+        count = 0 # count to track len of os.listdir(PATH)
+        for f in os.listdir(PATH):
+            count += 1
+            if regex.match(f) != None:
+                print('File Found!')
+                filename = regex.match(f).string
+                os.rename(PATH+filename, PATH+new_filename)
+                break
+            elif regex.match(f) == None and count == len(os.listdir(PATH)):
+                print('File not found')
+            else:
+                print('File does not match regex. Checking next file')
+        return
 
 
 
 
 if __name__ == '__main__':
-	geckodriver='/PATH/to/geckodriver'
+    geckodriver='/PATH/to/geckodriver'
 
-	username = ''
-	password = ''
-	report = ''
-	dl_dir = ''
+    username = ''
+    password = ''
+    report = ''
+    dl_dir = ''
 
-	ekos = EkosExport(
-		'Firefox',
-		geckodriver,
-		2,
-		dl_dir, # TODO: assert ends in /
-		headless=False
-	)
+    ekos = EkosExport(
+        'Firefox',
+        geckodriver,
+        2,
+        dl_dir, # TODO: assert ends in /
+        headless=False
+    )
 
-	ekos.login(ekos.session,username,password)
-	ekos.download_report(ekos.session, report)
-	ekos.rename_file('{}.csv'.format(report))
-	ekos.quit(ekos.session)
+    ekos.login(ekos.session,username,password)
+    ekos.download_report(ekos.session, report)
+    ekos.rename_file('{}.csv'.format(report))
+    ekos.quit(ekos.session)
 
 
